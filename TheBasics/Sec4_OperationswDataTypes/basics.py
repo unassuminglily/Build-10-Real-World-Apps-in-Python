9336ee0cc4250c9a6f937d13de30d89e3d167fc3
Lily <52928944+unassuminglily@users.noreply.github.com>
2021-06-06 03:02:44 +0000

    lists pt1

---
+++ TheBasics/Sec4_OperationswDataTypes/basics.py
@@ -0,0 +1,19 @@
+# List operations
+# codng exercise 1 Append Item to list
+seconds = [1.2323442655, 1.4534345567, 1.023458894]
+current = 1.10001399445
+seconds.append(current)
+print(seconds)
+
+# Coding Exercise 2 remove item from list
+seconds = [1.2323442655, 1.4534345567, 1.023458894, 1.10001399445]
+seconds.remove(seconds[1])
+print(seconds)
+
+# fix this with a loop. Exercise 3-- remove three items
+second = [1.2323442655, 1.4534345567, 1.023458894, 1.10001399445]
+second.remove(second[1])
+second.remove(second[1])
+second.remove(second[1])
+print(second)
+
